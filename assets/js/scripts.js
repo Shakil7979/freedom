38b6f086d7a73a98a702e7895729d46eb1209f91
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2024-09-23 16:03:04 +0600

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,27 +7,13 @@ $(document).ready(function(){
 	// });
 
 	// Banner Carousel
-	$('.expert_owl_carousel').owlCarousel({
-		items:3,
-		loop:true,
-		nav:false,
-		dots:true,
-		autoplay:true,
-		responsive : { 
-			0 : { 
-				items:1,
-			}, 
-			480 : {  
-				items:2,
-			}, 
-			768 : { 
-				items:2,
-			},
-			991 : { 
-				items:3,
-			}
-		}
-	});
+	// $('.banner_carousel').owlCarousel({
+	// 	items:1,
+	// 	loop:true,
+	// 	nav:true,
+	// 	dots:true,
+	// 	autoplay:true,
+	// });
 
 
 
